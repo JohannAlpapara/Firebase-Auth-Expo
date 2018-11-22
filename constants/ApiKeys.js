949481bfb443af9398990dcd09045042ffe2bc2f
JohannAlpapara <johannpaulalpapara@gmail.com>
2018-11-22 09:42:09 +0800

    Inital working app

---
+++ constants/ApiKeys.js
@@ -0,0 +1,10 @@
+export default {
+	FirebaseConfig: {
+		apiKey: "AIzaSyCKN2P0_rQu11ysz4mAP_F7O9Z58GkCqDQ",
+    authDomain: "expoauth-cb2b8.firebaseapp.com",
+    databaseURL: "https://expoauth-cb2b8.firebaseio.com",
+    projectId: "expoauth-cb2b8",
+    storageBucket: "expoauth-cb2b8.appspot.com",
+    messagingSenderId: "114074699969"
+	}
+}
